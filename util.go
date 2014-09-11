ffda8df95b09d1f138738513bd50cd5c0eb20654
wandergeek <innernetz@gmail.com>
2014-09-11 14:34:14 +1000

    added get interface function

--- util.go
+++ util.go
@@ -4,9 +4,12 @@ import (
 	"bytes"
 	"crypto/md5"
 	"encoding/hex"
+	"fmt"
 	"io/ioutil"
+	"net"
 	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/bitly/go-simplejson"
 )
@@ -94,4 +97,37 @@ func getDriverInfo(filename string) (*simplejson.Json, error) {
 
 	js.Del("scripts")
 	return js, nil
+}
+
+func GetNetAddress() (string, error) {
+	var ipAddr string
+
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		fmt.Errorf("Failed to get interfaces: %s", err)
+		return "", err
+	}
+
+	for _, i := range ifaces {
+		addrs, err := i.Addrs()
+		if err != nil {
+			fmt.Errorf("Failed to get addresses: %s", err)
+			return "", err
+		}
+		for _, addr := range addrs {
+			switch v := addr.(type) {
+			case *net.IPNet:
+				if addr.String() != "127.0.0.1/8" && addr.String() != "::1/128" {
+					rawAddy := addr.String()
+					ipAddr = strings.Split(rawAddy, "/")[0]
+				}
+			default:
+				fmt.Printf("unexpected type %T", v) // %T prints whatever type t has
+			}
+
+		}
+	}
+
+	return ipAddr, nil
+
 }
