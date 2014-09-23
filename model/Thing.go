466eafb05d33fac6b83440d60779040e5aa7b20f
Elliot Shepherd <elliot@ninjablocks.com>
2014-09-23 15:50:47 +1000

    omitempty not omitEmpty :)

--- model/Thing.go
+++ model/Thing.go
@@ -3,8 +3,8 @@ package model
 type Thing struct {
 	Name     string  `json:"name" redis:"name"`
 	ID       string  `json:"id" redis:"id"`
-	Device   *Device `json:"device,omitEmpty" redis:"-"`
+	Device   *Device `json:"device,omitempty" redis:"-"`
 	DeviceID *string `json:"-" redis:"device"`
 	Type     string  `json:"type" redis:"type"`
-	Location *string `json:"location,omitEmpty" redis:"location"`
+	Location *string `json:"location,omitempty" redis:"location"`
 }
