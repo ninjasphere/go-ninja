ddd9d6b04ac023936cfe4dc417b0113e30c1dec0
Elliot Shepherd <elliot@ninjablocks.com>
2014-09-23 15:48:33 +1000

    Config package to load from sphere-config and sphere-serial

---
+++ config/config.go
@@ -0,0 +1,60 @@
+package config
+
+import (
+	"bytes"
+	"log"
+	"os/exec"
+
+	"github.com/bitly/go-simplejson"
+)
+
+var cfg = mustLoadConfig()
+
+// MustString returns the string property at the path
+func MustString(path ...string) string {
+	return cfg.GetPath(path...).MustString()
+}
+
+// MustInt returns the string property at the path
+func MustInt(path ...string) int {
+	return cfg.GetPath(path...).MustInt()
+}
+
+// MustBool returns the string property at the path
+func MustBool(path ...string) bool {
+	return cfg.GetPath(path...).MustBool()
+}
+
+func Serial() string {
+
+	cmd := exec.Command("sphere-serial")
+
+	var out bytes.Buffer
+	cmd.Stdout = &out
+
+	err := cmd.Run()
+	if err != nil {
+		log.Fatalf("Failed to get sphere serial (sphere-serial must be in the PATH) error:%s", err)
+	}
+
+	return out.String()
+}
+
+// MustLoadConfig parses the output of "sphere-config"
+func mustLoadConfig() *simplejson.Json {
+	cmd := exec.Command("sphere-config")
+
+	var out bytes.Buffer
+	cmd.Stdout = &out
+
+	err := cmd.Run()
+	if err != nil {
+		log.Fatalf("Failed to load configuration. ('sphere-config' must be in the PATH) error:%s", err)
+	}
+
+	cfg, err := simplejson.NewJson(out.Bytes())
+	if err != nil {
+		log.Fatalf("Failed to parse configuration. error:%s", err)
+	}
+	return cfg
+}
