07739178c83a624bdd93099438d9bd6efc04c3cd
Elliot Shepherd <elliot@ninjablocks.com>
2014-09-26 12:06:36 +1000

    ServiceClient.OnEvent

--- api/ServiceClient.go
+++ api/ServiceClient.go
@@ -1,10 +1,20 @@
 package ninja
 
-import "time"
+import (
+	"time"
+
+	"git.eclipse.org/gitroot/paho/org.eclipse.paho.mqtt.golang.git"
+)
 
 type ServiceClient struct {
 	conn  *Connection
 	topic string
+}
+
+func (c *ServiceClient) OnEvent(event string, callback func(message mqtt.Message) bool) error {
+	return c.conn.Subscribe(c.topic+"/event/"+event, func(message mqtt.Message, values map[string]string) bool {
+		return callback(message)
+	})
 }
 
 func (c *ServiceClient) Call(method string, args interface{}, reply interface{}, timeout time.Duration) error {
