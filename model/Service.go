3811f877a69e82b6695ae5665b6ed653bf612a5b
Elliot Shepherd <elliot@ninjablocks.com>
2014-09-29 11:26:55 +1000

    Temporarily also export channels under the old $device/:id/channel/:name/:protocol topic

--- model/Service.go
+++ model/Service.go
@@ -3,6 +3,7 @@ package model
 type ServiceAnnouncement struct {
 	Topic            string    `json:"topic" redis:"topic"`
 	Schema           string    `json:"schema" redis:"schema"`
+	Deprecated       *bool     `json:"deprecated,omitempty"`
 	SupportedMethods *[]string `json:"methods" redis:"methods,json"`
 	SupportedEvents  *[]string `json:"events" redis:"events,json"`
 }
