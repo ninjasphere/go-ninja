9de234f1e210a18ae8c59ce8cd55091e60b15208
Elliot Shepherd <elliot@ninjablocks.com>
2014-10-02 18:46:31 +1000

    Remove ServiceAnnouncement.Deprecated prop. Not needed.

--- model/Service.go
+++ model/Service.go
@@ -3,7 +3,6 @@ package model
 type ServiceAnnouncement struct {
 	Topic            string    `json:"topic" redis:"topic"`
 	Schema           string    `json:"schema" redis:"schema"`
-	Deprecated       *bool     `json:"deprecated,omitempty"`
 	SupportedMethods *[]string `json:"supportedMethods" redis:"supportedMethods,json"`
 	SupportedEvents  *[]string `json:"supportedEvents" redis:"supportedEvents,json"`
 }
