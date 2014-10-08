c24a568b2f59884f2a6dd0d2cfc0bdfb8103f6dc
Mark Wolfe <mark@wolfe.id.au>
2014-10-08 23:25:05 +1100

    Added state to the model for use in the REST services.

--- model/Channel.go
+++ model/Channel.go
@@ -2,9 +2,10 @@ package model
 
 type Channel struct {
 	ServiceAnnouncement
-	ID       string  `json:"id" redis:"id"`
-	Protocol string  `json:"protocol" redis:"protocol"`
-	Device   *Device `json:"device" redis:"-"`
+	ID        string      `json:"id" redis:"id"`
+	Protocol  string      `json:"protocol" redis:"protocol"`
+	Device    *Device     `json:"device" redis:"-"`
+	LastState interface{} `json:"lastState" redis:"-"` // used to store the last state of the channel
 }
 
 func (c *Channel) GetServiceAnnouncement() *ServiceAnnouncement {
