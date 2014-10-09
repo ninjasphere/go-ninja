f0577d37fe5bb795de6cfb5a2ffad78057d0fc1d
Elliot Shepherd <elliot@ninjablocks.com>
2014-10-09 14:08:29 +1100

    Fix json en/decoding and redis storage of thing/device relationship

--- model/Device.go
+++ model/Device.go
@@ -7,7 +7,7 @@ type Device struct {
 	NaturalID     string             `json:"naturalId" redis:"naturalId"`
 	NaturalIDType string             `json:"naturalIdType" redis:"naturalIdType"`
 	Name          *string            `json:"name,omitempty" redis:"name"`
-	Thing         *string            `json:"thing,omitempty" redis:"thing"`
+	ThingID       *string            `json:"thing,omitempty" redis:"-"`
 	Channels      *[]*Channel        `json:"channels,omitempty" redis:"-"`
 	Signatures    *map[string]string `json:"signatures,omitempty" redis:"signatures,json"`
 }
