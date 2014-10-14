7d73e2043541aa737dfb865b2c3dc2180dc25ea6
Elliot Shepherd <elliot@ninjablocks.com>
2014-10-15 08:39:42 +1100

    Return error if ServiceClient.Call given non-nil reply with timeout=0

--- api/ServiceClient.go
+++ api/ServiceClient.go
@@ -1,6 +1,7 @@
 package ninja
 
 import (
+	"fmt"
 	"time"
 )
 
@@ -15,22 +16,26 @@ type ServiceClient struct {
 // 	err := sm.conn.GetServiceClient("$device/:deviceid/channel/:channelid")
 //                     .OnEvent("state", func(params *YourEventType, topicKeys map[string]string) bool {
 //  	..
-//	    return true	  
+//	    return true
 //	})
 //
 // YourEventType must either be *json.RawMessage or a pointer to go type to which the raw JSON message can successfully be unmarshalled.
-// 
+//
 // There is one entry in the topicKeys map for each parameter marker in the topic string used to obtain the ServiceClient.
 //
 // Both the params and topicKeys parameters can be omitted. If the topicKeys parameter is required, the params parameter must also be specified.
 //
 func (c *ServiceClient) OnEvent(event string, callback interface{}) error {
-	return c.conn.Subscribe(c.topic+"/event/"+event, callback);
+	return c.conn.Subscribe(c.topic+"/event/"+event, callback)
 }
 
 func (c *ServiceClient) Call(method string, args interface{}, reply interface{}, timeout time.Duration) error {
 	if timeout > 0 {
 		return c.conn.rpc.CallWithTimeout(c.topic, method, args, reply, timeout)
+	}
+
+	if reply != nil {
+		return fmt.Errorf("Attempted async call to method %s on service %s with a non-nil reply", method, c.topic)
 	}
 
 	return c.conn.rpc.Call(c.topic, method, args)
