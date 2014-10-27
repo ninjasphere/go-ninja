5c6e42d567a61a969f65e18d64323254a31f2583
Jon Seymour <jon@ninjablocks.com>
2014-10-27 16:17:42 +1100

    Add WaitUntilSignal to encapsulate pattern used by many drivers and apps.

---
+++ support/runtime.go
@@ -0,0 +1,18 @@
+package support
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+)
+
+// WaitUntilSignal forces the caller to wait until an OS signal is received. This is typically called at the base of the main function
+// of a go process.
+func WaitUntilSignal() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, os.Kill)
+
+	// Block until a signal is received.
+	s := <-c
+	fmt.Println("Got signal:", s)
+}
