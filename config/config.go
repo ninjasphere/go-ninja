d05f13f4334ab70d727e0729e9010e903c849d75
Jon Seymour <jon@ninjablocks.com>
2014-11-01 15:20:21 +1100

    Support for getting Ints with default.

--- config/config.go
+++ config/config.go
@@ -31,6 +31,11 @@ func Bool(def bool, path ...string) bool {
 	return cfg.GetPath(path...).MustBool(def)
 }
 
+// Int returns the integer property at the path, with a default
+func Int(def int, path ...string) int {
+	return cfg.GetPath(path...).MustInt(def)
+}
+
 func String(def string, path ...string) string {
 	return cfg.GetPath(path...).MustString(def)
 }
