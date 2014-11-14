975cb6b43b5ea8ad2784ca3f5da141e9a26a129e
Elliot Shepherd <elliot@ninjablocks.com>
2014-11-14 16:03:44 +1100

    Replace node sphere-config with >20x faster go version

--- config/config.go
+++ config/config.go
@@ -2,53 +2,31 @@ package config
 
 import (
 	"bytes"
-	"log"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"os/user"
+	"path/filepath"
+	"reflect"
+	"strconv"
+	"strings"
 
-	"github.com/bitly/go-simplejson"
+	"github.com/ninjasphere/go-ninja/logger"
+	"gopkg.in/alecthomas/kingpin.v1"
 )
 
-var cfg *simplejson.Json
+var log = logger.GetLogger("config")
 
-// MustString returns the string property at the path
-func MustString(path ...string) string {
-	nonce := "$splinereticulationid$"
-	val := cfg.GetPath(path...).MustString(nonce)
-	if val == nonce {
-		log.Fatalf("expected value for %v but found nothing", path)
-	}
-	return val
+var config map[string]interface{}
+
+func init() {
+	MustRefresh()
 }
 
-// MustInt returns the string property at the path
-func MustInt(path ...string) int {
-	return cfg.GetPath(path...).MustInt()
-}
-
-// MustBool returns the boolean property at the path
-func MustBool(path ...string) bool {
-	return cfg.GetPath(path...).MustBool()
-}
-
-// Bool returns the boolean property at the path, with a default
-func Bool(def bool, path ...string) bool {
-	return cfg.GetPath(path...).MustBool(def)
-}
-
-// Int returns the integer property at the path, with a default
-func Int(def int, path ...string) int {
-	return cfg.GetPath(path...).MustInt(def)
-}
-
-func String(def string, path ...string) string {
-	return cfg.GetPath(path...).MustString(def)
-}
-
-var hey = "what's up buddy?"
-
-func HasString(path ...string) bool {
-	return String(hey, path...) != hey
+func GetAll() map[string]interface{} {
+	return unflatten(config)
 }
 
 var serial string
@@ -71,24 +49,259 @@ func Serial() string {
 	return serial
 }
 
-// MustRefresh parses the output of "sphere-config"
-func MustRefresh() {
-	cmd := exec.Command("sphere-config", os.Args[1:]...)
+func String(def string, path ...string) string {
+	val := get(path...)
+	if val == nil {
+		return def
+	}
+	return val.(string)
+}
 
-	var out bytes.Buffer
-	cmd.Stdout = &out
+// MustString returns the string property at the path
+func MustString(path ...string) string {
+	return mustGet(path...).(string)
+}
 
-	err := cmd.Run()
-	if err != nil {
-		log.Fatalf("Failed to load configuration. ('sphere-config' must be in the PATH) error:%s", err)
+// Int returns the integer property at the path, with a default
+func Int(def int, path ...string) int {
+	val := get(path...)
+	if val == nil {
+		return def
+	}
+	return int(val.(float64))
+}
+
+// MustInt returns the string property at the path
+func MustInt(path ...string) int {
+	return int(mustGet(path...).(float64))
+}
+
+// Bool returns the boolean property at the path, with a default
+func Bool(def bool, path ...string) bool {
+	val := get(path...)
+	if val == nil {
+		return def
+	}
+	return val.(bool)
+}
+
+// MustBool returns the boolean property at the path
+func MustBool(path ...string) bool {
+	return mustGet(path...).(bool)
+}
+
+var hey = "what's up buddy?"
+
+func HasString(path ...string) bool {
+	val, ok := config[strings.Join(path, ".")]
+	if !ok {
+		return false
 	}
 
-	cfg, err = simplejson.NewJson(out.Bytes())
-	if err != nil {
-		log.Fatalf("Failed to parse configuration. error:%s", err)
+	_, ok = val.(string)
+	return ok
+}
+
+func mustGet(path ...string) interface{} {
+	val, ok := config[strings.Join(path, ".")]
+	if !ok {
+		log.Fatalf("expected value for %v but found nothing", path)
+	}
+	return val
+}
+
+func get(path ...string) interface{} {
+	val, ok := config[strings.Join(path, ".")]
+	if !ok {
+		return nil
+	}
+	return val
+}
+
+func MustRefresh() {
+
+	flat := make(map[string]interface{})
+
+	// cli overrides
+	addArgs(flat)
+
+	// load environments (no value args) from cli args
+	environments := []string{"default"}
+	for name, value := range flat {
+		if value == nil {
+			environments = append(environments, name)
+		}
+	}
+
+	flat["env"] = environments
+
+	userHome := getUserHome()
+
+	// env vars (if starting with "sphere_")
+	addEnv(flat)
+
+	// anything that can be parsed as a number, is a number
+	parseNumbers(flat)
+
+	installDir := "/opt/ninjablocks"
+	if val, ok := flat["installDirectory"]; ok {
+		installDir = val.(string)
+	}
+
+	flat["installDirectory"] = installDir
+
+	//sphere_configDir=/home/elliot/repos/sphere-dev/sphere-common/config
+
+	if _, err := os.Stat(installDir); err != nil {
+		log.Fatalf("Couldn't load sphere install directory. Override with env var sphere_installDirectory. error:%s", err)
+	}
+
+	// credentials file
+	addFile("/etc/opt/ninja/credentials.json", flat)
+
+	// home directory environment(s) config
+	for i := len(environments) - 1; i >= 0; i-- {
+		addFile(filepath.Join(userHome, ".sphere", environments[i]+".json"), flat)
+	}
+
+	// current directory environment(s) config
+	for i := len(environments) - 1; i >= 0; i-- {
+		addFile(filepath.Join(".", "config", environments[i]+".json"), flat)
+	}
+
+	// common directory environment(s) config
+	for i := len(environments) - 1; i >= 0; i-- {
+		addFile(filepath.Join(installDir, "sphere-common", "config", environments[i]+".json"), flat)
+	}
+
+	// sphere-common credentials config
+	addFile(filepath.Join(installDir, "sphere-common", "config", "credentials.json"), flat)
+
+	//log.Debugf("Loaded config: %v", flat)
+
+	config = flat
+}
+
+func addEnv(config map[string]interface{}) {
+	for _, v := range os.Environ() {
+		split := strings.Split(v, "=")
+
+		name, value := split[0], split[1]
+
+		if strings.HasPrefix(name, "sphere_") {
+			name = strings.TrimPrefix(name, "sphere_")
+			name = strings.Replace(name, "_", ".", 0)
+
+			if _, ok := config[name]; !ok {
+				config[name] = value
+			}
+		}
 	}
 }
 
-func init() {
-	MustRefresh()
+func addArgs(config map[string]interface{}) {
+
+	parser := kingpin.Tokenize(os.Args[1:]).Tokens
+
+	for token, parser := parser.Peek(), parser.Next(); token.Type != kingpin.TokenEOL; token, parser = parser.Peek(), parser.Next() {
+
+		if token.IsFlag() {
+			var value interface{}
+			name := token.Value
+
+			next := parser.Peek()
+			if next.Type == kingpin.TokenArg {
+				value = next.Value
+			} else {
+				// It's an environment indicator... like --cloud-production
+			}
+
+			if _, ok := config[name]; !ok {
+				config[name] = value
+			}
+		}
+
+	}
+
+}
+
+func addFile(path string, config map[string]interface{}) error {
+	//log.Debugf("Loading config file: %s", path)
+
+	file, e := ioutil.ReadFile(path)
+	if e != nil {
+		return fmt.Errorf("Failed to load file: %s error: %s", path, e)
+	}
+
+	content := make(map[string]interface{})
+	e = json.Unmarshal(file, &content)
+	if e != nil {
+		return fmt.Errorf("Failed to read file: %s error: %s", path, e)
+	}
+
+	flatten(content, nil, config)
+	return nil
+}
+
+func flatten(input interface{}, lpath []string, flattened map[string]interface{}) {
+	if lpath == nil {
+		lpath = []string{}
+	}
+
+	if reflect.ValueOf(input).Kind() == reflect.Map {
+		for rkey, value := range input.(map[string]interface{}) {
+			flatten(value, append(lpath, rkey), flattened)
+		}
+	} else {
+		if _, ok := flattened[strings.Join(lpath, ".")]; !ok {
+			flattened[strings.Join(lpath, ".")] = input
+		}
+	}
+}
+
+func unflatten(flat map[string]interface{}) map[string]interface{} {
+	out := make(map[string]interface{})
+	obj := out
+
+	for key, val := range flat {
+
+		obj = out
+		keys := strings.Split(key, ".")
+
+		for i, k := range keys {
+			if i == len(keys)-1 {
+				obj[k] = val
+			} else {
+				next, ok := obj[k]
+				if !ok {
+					next = make(map[string]interface{})
+				}
+				obj[k] = next
+				obj = next.(map[string]interface{})
+			}
+		}
+
+	}
+
+	return out
+}
+
+func parseNumbers(config map[string]interface{}) {
+	for name, val := range config {
+		stringVal, ok := val.(string)
+		if ok {
+			floatVal, err := strconv.ParseFloat(stringVal, 64)
+			if err == nil {
+				config[name] = floatVal
+			}
+		}
+	}
+}
+
+func getUserHome() string {
+	usr, err := user.Current()
+	if err != nil {
+		return "/root"
+	}
+	return usr.HomeDir
 }
