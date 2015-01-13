686b8731b42bd8775ba2e7c8bac54b9e7c838164
☃ Elliot Shepherd <elliot@ninjablocks.com>
2015-01-13 17:11:46 +1100

    TinyBus - Add auto-reconnect + last will and testament

--- bus/TinyBus.go
+++ bus/TinyBus.go
@@ -3,41 +3,112 @@ package bus
 import (
 	"fmt"
 	"net"
+	"sync"
+	"time"
 
 	proto "github.com/huin/mqtt"
 	"github.com/jeffallen/mqtt"
+	"github.com/ninjasphere/go-ninja/config"
 )
 
 type TinyBus struct {
+	connecting    sync.WaitGroup
 	mqtt          *mqtt.ClientConn
 	subscriptions []*subscription
+	host          string
+	id            string
 }
 
 func ConnectTinyBus(host, id string) (*TinyBus, error) {
-	conn, err := net.Dial("tcp", host)
-	if err != nil {
-		return nil, fmt.Errorf("Failed to connect to: %s", err)
-	}
-	mqtt := mqtt.NewClientConn(conn)
-	mqtt.ClientId = id
-
-	err = mqtt.Connect("", "")
-	if err != nil {
-		return nil, fmt.Errorf("MQTT Failed to connect to: %s", err)
-	}
 
 	bus := &TinyBus{
-		mqtt:          mqtt,
 		subscriptions: make([]*subscription, 0),
+		host:          host,
+		id:            id,
+	}
+
+	bus.connect()
+
+	return bus, nil
+}
+
+type wrappedConn struct {
+	net.Conn
+	done chan bool
+}
+
+func (c wrappedConn) Close() error {
+	log.Warningf("Connection closed!")
+	c.done <- true
+	c.Conn.Close()
+	return nil
+}
+
+func (b *TinyBus) connect() {
+
+	b.connecting.Add(1)
+	defer func() {
+		b.connecting.Done()
+
+		b.publish(&proto.Publish{
+			Header: proto.Header{
+				Retain: true,
+			},
+			TopicName: fmt.Sprintf("node/%s/module/%s/state/connected", config.Serial(), b.id),
+			Payload:   proto.BytesPayload([]byte("true")),
+		})
+	}()
+
+	var conn wrappedConn
+	for {
+		tcpConn, err := net.Dial("tcp", b.host)
+		if err == nil {
+			conn = wrappedConn{
+				Conn: tcpConn,
+				done: make(chan bool, 1),
+			}
+			break
+		}
+
+		//log.Warningf("Failed to connect to: %s", err)
+		time.Sleep(time.Millisecond * 500)
+	}
+
+	if b.mqtt != nil {
+		log.Infof("Reconnected to mqtt server")
+	}
+
+	mqtt := mqtt.NewClientConn(conn)
+	mqtt.ClientId = b.id
+
+	err := mqtt.ConnectCustom(&proto.Connect{
+		WillFlag:    true,
+		WillQos:     0,
+		WillRetain:  true,
+		WillTopic:   fmt.Sprintf("$node/%s/module/%s/state/connected", config.Serial(), b.id),
+		WillMessage: "false",
+	})
+
+	if err != nil {
+		log.Fatalf("MQTT Failed to connect to: %s", err)
+	}
+
+	b.mqtt = mqtt
+
+	for _, s := range b.subscriptions {
+		b.subscribe(s)
 	}
 
 	go func() {
-		for m := range bus.mqtt.Incoming {
-			bus.onIncoming(m)
+		for m := range mqtt.Incoming {
+			b.onIncoming(m)
 		}
 	}()
 
-	return bus, nil
+	go func() {
+		<-conn.done
+		b.connect()
+	}()
 }
 
 func (b *TinyBus) onIncoming(message *proto.Publish) {
@@ -49,12 +120,17 @@ func (b *TinyBus) onIncoming(message *proto.Publish) {
 }
 
 func (b *TinyBus) Publish(topic string, payload []byte) {
+	b.connecting.Wait()
 
-	b.mqtt.Publish(&proto.Publish{
+	b.publish(&proto.Publish{
 		TopicName: topic,
 		Payload:   proto.BytesPayload(payload),
 	})
 
+}
+
+func (b *TinyBus) publish(message *proto.Publish) {
+	b.mqtt.Publish(message)
 }
 
 func (b *TinyBus) Subscribe(topic string, callback func(topic string, payload []byte)) (*subscription, error) {
