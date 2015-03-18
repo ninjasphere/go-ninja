4ae2870621073321470934cf498641ce7181171a
Jon Seymour <jon@ninjablocks.com>
2015-03-18 20:50:08 +1100

    Add a utility function, PostConstruct, to the support package.
    This method will invoke the PostConstruct method of any object that implements the interface in the order specified by the caller. If a single
    method fails, the loop is aborted and the error returned.
    
    Signed-off-by: Jon Seymour <jon@ninjablocks.com>

---
+++ support/PostConstruct.go
@@ -0,0 +1,26 @@
+package support
+
+import (
+	"fmt"
+	"reflect"
+)
+
+// Types that need to do something post injecction should implement a PostConstruct method as per this
+// interface and will then my notified post constructionn via a call to that method.
+type PostConstruct interface {
+	PostConstruct() error
+}
+
+// Given a list of injectable things, call the PostConstruct method of each if there is one. Fail on the first
+// one that fails.
+func PostConstructAll(injectables []interface{}) error {
+	// Run PostConstruct on any objects that have it
+	for _, node := range injectables {
+		if pc, ok := node.(PostConstruct); ok {
+			if err := pc.PostConstruct(); err != nil {
+				return fmt.Errorf("Failed PostConstruct on object %s: %s", reflect.TypeOf(pc).String(), err)
+			}
+		}
+	}
+	return nil
+}
