0e1c34ad3af773d7fe8399bbcd0181d471949566
Jon Seymour <jon@ninjablocks.com>
2015-10-02 15:18:27 +1000

    Extend site model with storage for a defaultRoomID.
    This is used in conjunction with autopromote to allow homecloud
    to auto-promote all newly created devices.
    
    Signed-off-by: Jon Seymour <jon@ninjablocks.com>

--- model/Site.go
+++ model/Site.go
@@ -10,6 +10,7 @@ type Site struct {
 	TimeZoneName    *string     `json:"timeZoneName,omitempty" redis:"timeZoneName"`
 	TimeZoneOffset  *int        `json:"timeZoneOffset,omitempty" redis:"timeZoneOffset"`
 	SitePreferences interface{} `json:"site-preferences,omitempty" redis:"site-preferences,json"`
+	DefaultRoomID   *string     `json:"defaultRoomId,omitempty" redis:"defaultRoomId,json"`
 }
 
 //https://maps.googleapis.com/maps/api/timezone/json?location=-33.86,151.20&timestamp=1414645501
