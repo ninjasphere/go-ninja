f2efe7ca67ca2b0e0601fe53171ce9a652c1ed27
Jon Seymour <jon@ninjablocks.com>
2015-10-06 12:27:24 +1100

    Remove SphereAutoRegisterUserId and RegisterSphere.
    Signed-off-by: Jon Seymour <jon@ninjablocks.com>

--- cloud/cloud.go
+++ cloud/cloud.go
@@ -37,29 +37,3 @@ func ChangePassword(accessToken string, newPassword string) error {
 func ActivateSphere(accessToken string, nodeId string) error {
 	return fmt.Errorf("not implemented: ActivateSphere")
 }
-
-/**
- * Answers the userid used by RegisterSphere.
- *
- * Will typically be a string of the form sphere+{nodeId}@ninjablocks.com but
- * this can be varied by configuration.
- */
-func SphereAutoRegisterUserId(nodeId string) string {
-	return fmt.Sprintf("sphere+%s@ninjablocks.com", nodeId)
-}
-
-/**
- * Registers the current sphere with a unique userid for the sphere
- * which is generated from the sphere serial id.
- *
- * The password is only required on a second attempt to auto-register the sphere.
- *
- * This performs the equivalent of:
- *
- * - RegisterUser("Default User", SphereAutoRegisterUserId("serial"), "{random-password}")
- * - token = AuthenticateUser(SphereAutoRegisterUserId("serial"), "{random-password}")
- * - ActivateSphere(token, "{serial}")
- */
-func RegisterSphere(password *string) error {
-	return fmt.Errorf("not implemented: RegisterSphere")
-}
