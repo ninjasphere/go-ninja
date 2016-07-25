9ce57fbcbca565916398e1c11623b87fd032edfb
Jon Seymour <jon@wildducktheories.com>
2016-07-25 14:41:48 +1000

    Add a mutex to guard access to the ticks map.
    The need to do this became apparent once compiling with go 1.6 which
    it now does more aggressive race condition checks.
    
    Signed-off-by: Jon Seymour <jon@wildducktheories.com>

--- logger/stats.go
+++ logger/stats.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"sort"
 	"time"
+	"sync"
 )
 
 var tickLog = GetLogger("stats")
@@ -12,6 +13,7 @@ var tickLog = GetLogger("stats")
 var ReportInterval = time.Second * 5
 
 var ticks = make(map[string]int)
+var mu = sync.Mutex{}
 
 func init() {
 	go func() {
@@ -24,18 +26,22 @@ func init() {
 
 			ticks2 := make(map[string]int)
 
+			mu.Lock()
 			keys := make([]string, 0, len(ticks))
 			for key := range ticks {
 				keys = append(keys, key)
 			}
+			mu.Unlock()
 			sort.Strings(keys)
 
 			var msg string
+			mu.Lock()
 			for _, name := range keys {
 				ticks2[name] = 0
 				count := ticks[name]
 				msg += fmt.Sprintf("[%s: %.f/sec] ", name, float64(count)/(float64(ReportInterval)/float64(time.Second)))
 			}
+			mu.Unlock()
 
 			ticks = ticks2
 
@@ -50,6 +56,9 @@ func Tick(name string) {
 }
 
 func TickN(name string, number int) {
+	mu.Lock()
+	defer mu.Unlock()
+
 	if _, ok := ticks[name]; !ok {
 		ticks[name] = 0
 	}
