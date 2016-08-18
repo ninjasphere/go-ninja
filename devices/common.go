1f2bdb3593da3577dbc4492e8378360fe5ecd2b7
Jon Seymour <jon@wildducktheories.com>
2016-08-18 11:48:25 +1000

    BaseDevice is a re-usable outside the package, baseDevice is not.
    Signed-off-by: Jon Seymour <jon@wildducktheories.com>

--- devices/common.go
+++ devices/common.go
@@ -29,3 +29,32 @@ func (d *baseDevice) SetEventHandler(sendEvent func(event string, payload interf
 func (d *baseDevice) Log() *logger.Logger {
 	return d.log
 }
+
+// Re-expressed with public members so that drivers that don't want to use the Device structs devices.*
+// can do so without re-implementing all the Device methods themselves.
+
+type BaseDevice struct {
+	Driver ninja.Driver
+	Info   *model.Device
+
+	Conn *ninja.Connection
+	Log_ *logger.Logger
+
+	SendEvent func(event string, payload interface{}) error
+}
+
+func (d *BaseDevice) GetDeviceInfo() *model.Device {
+	return d.Info
+}
+
+func (d *BaseDevice) GetDriver() ninja.Driver {
+	return d.Driver
+}
+
+func (d *BaseDevice) SetEventHandler(sendEvent func(event string, payload interface{}) error) {
+	d.SendEvent = sendEvent
+}
+
+func (d *BaseDevice) Log() *logger.Logger {
+	return d.Log_
+}
