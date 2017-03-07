c6e774b9fac24f19a81a8ddfe7f708a8a4bd4aae
Jon Seymour <jon@wildducktheories.com>
2017-03-08 09:58:59 +1100

    Add support for definining channels in differenct packages

--- channels/common.go
+++ channels/common.go
@@ -12,3 +12,16 @@ func (c *baseChannel) SetEventHandler(handler func(event string, payload ...inte
 func (c *baseChannel) GetProtocol() string {
 	return c.protocol
 }
+
+type BaseChannel struct {
+	Protocol  string
+	SendEvent func(event string, payload ...interface{}) error
+}
+
+func (c *BaseChannel) SetEventHandler(handler func(event string, payload ...interface{}) error) {
+	c.SendEvent = handler
+}
+
+func (c *BaseChannel) GetProtocol() string {
+	return c.Protocol
+}
